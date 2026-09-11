9954ae4c13c08d639b8efce958ce432353ec0607
Raunak <raunak2312clg@gmail.com>
2026-09-12 00:48:56 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/PARASHOOT/',
+
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
       '/api': 'http://localhost:3001'
     }
   },
+
   preview: {
     port: 4173
   }
